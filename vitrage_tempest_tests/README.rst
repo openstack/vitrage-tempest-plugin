76c539d02ca683cec5ba3c0c5205ad65314a9a80
MarinaKoushnir <marina.koushnir@alcatel-lucent.com>
2015-12-14 14:04:29 +0000

    init vitrage tempest plugin
    Change-Id: I34df991312cba90e79d31e3e52836df6de1f8847

---
+++ vitrage_tempest_tests/README.rst
@@ -0,0 +1,6 @@
+==============================
+Tempest Integration of Vitrage
+==============================
+
+This directory contains Tempest tests to cover the Vitrage project.
+
---
+++ vitrage_tempest_tests/config.py
@@ -0,0 +1,43 @@
+# Copyright 2015
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from oslo_config import cfg
+
+service_available_group = cfg.OptGroup(name="service_available",
+                                       title="Available OpenStack Services")
+
+ServiceAvailableGroup = [
+    cfg.BoolOpt("vitrage",
+                default=True,
+                help="Whether or not vitrage is expected to be available"),
+]
+
+rca_engine_group = cfg.OptGroup(name="root_cause_analysis_engine",
+                                title="Root Cause Analysis Engine Options")
+
+RcaEngineGroup = [
+    # RCA Engine tempest configuration
+    cfg.StrOpt("region",
+               default="",
+               help="The application_catalog region name to use. If empty, "
+                    "the value of identity.region is used instead. "
+                    "If no such region is found in the service catalog, "
+                    "the first found one is used."),
+
+    cfg.StrOpt("identity_version",
+               default="v2",
+               help="Default identity version for "
+                    "REST client authentication.")
+]
