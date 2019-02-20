9b444b1c41f84fce56e7117a2dcef3affee52f19
Idan Hefetz <idan.hefetz@nokia.com>
2019-02-20 07:13:01 +0000

    Test template v3 with parameters
    Change-Id: I0c9c588b8233ed2824bc580f94b48b944328119d
    Story: 2004056
    Task: 29446

---
+++ vitrage_tempest_plugin/tests/api/templates/test_template_v3.py
@@ -0,0 +1,43 @@
+# Copyright 2019 - Nokia
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from oslo_log import log as logging
+
+from vitrage_tempest_plugin.tests.api.templates.test_template_v2 \
+    import TestTemplatesApis
+
+LOG = logging.getLogger(__name__)
+
+WITH_PARAMS_TEMPLATE = 'v3_with_params.yaml'
+
+
+class TestTemplatesV3(TestTemplatesApis):
+
+    def test_template_validate_with_missing_parameters(self):
+        self._validate_with_missing_parameters(WITH_PARAMS_TEMPLATE)
+
+    def test_template_validate_with_missing_parameter(self):
+        self._validate_with_missing_parameter(WITH_PARAMS_TEMPLATE)
+
+    def test_template_validate_with_parameters(self):
+        self._validate_with_parameters(WITH_PARAMS_TEMPLATE)
+
+    def test_template_add_with_missing_parameters(self):
+        self._add_with_missing_parameters(WITH_PARAMS_TEMPLATE)
+
+    def test_template_add_with_missing_parameter(self):
+        self._add_with_missing_parameter(WITH_PARAMS_TEMPLATE)
+
+    def test_template_add_with_parameters(self):
+        self._add_with_parameters(WITH_PARAMS_TEMPLATE)
