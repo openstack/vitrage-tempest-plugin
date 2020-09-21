6a6573af44611ed48c0db6e041a64708c50e5471
Eyal <eyal.bar-ilan@nokia.com>
2020-09-21 11:33:18 +0300

    fix swap size
    swap size was reduced to all nodes this created oom when running ceilometer
    increase the size to what it was before
    
    Change-Id: I8b86ba958bab07589abf6570f2242f7fc29bdb78

--- vitrage_tempest_plugin/tests/datasources/test_aodh.py
+++ vitrage_tempest_plugin/tests/datasources/test_aodh.py
@@ -13,7 +13,6 @@
 # under the License.
 
 from oslo_log import log as logging
-import testtools
 from vitrage_tempest_plugin.tests import utils
 
 from vitrage_tempest_plugin.tests.base import BaseVitrageTempest
@@ -24,7 +23,6 @@ from vitrage_tempest_plugin.tests.common.tempest_clients import TempestClients
 LOG = logging.getLogger(__name__)
 
 
-@testtools.skip('gnocchi is not supported')
 class TestAodhAlarm(BaseVitrageTempest):
     NUM_INSTANCE = 1
     NUM_ALARM = 1
